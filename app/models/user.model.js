ad3b969eb048d2c113a39df8e52eef62069026c8
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-10-23 21:26:40 +0530

    added:check if email exists or not

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,12 +1,18 @@
-//const pool = require('pg');
 const createUser = "INSERT INTO users (firstName,lastName,email,password) VALUES ($1,$2,$3,$4) RETURNING *";
-const loginUser = "SELECT * FROM users where email=$1 AND password=$2";
+const loginUser = "SELECT * FROM users WHERE email=$1 AND password=$2";
+const checkIfEmailExists = "SELECT * FROM users WHERE email=$1";
+const { query } = require("express");
 const pool = require("..//../config/database.config");
 
 
 class UserModel {
    createDetails = (userDetails,callback) => {
    console.log("inside model");
+   const query = [userDetails.email];
+   pool.query(checkIfEmailExists,query,(err,data)=>{
+    if (data.rows.length) {
+        callback("Email already exists");
+    }
    const query = [userDetails.firstName,userDetails.lastName,userDetails.email,userDetails.password];
        pool.query(createUser,query,(err,data)=>{
            if(err) {
@@ -17,7 +23,8 @@ class UserModel {
             callback(null,data.rows[0]);
             }
        });
-     }
+     });
+    };
      loginUser = (loginData, authenticateUser) => {
       console.log('inside model');
       const query = [loginData.email,loginData.password];
