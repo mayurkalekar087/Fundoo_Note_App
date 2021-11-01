13eb4fe50766502e454ddafc18c5581863c603b9
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-11-01 14:47:52 +0530

    added: test cases for forgot password api

--- test/test.js
+++ test/test.js
@@ -12,10 +12,10 @@ describe("User Registration ", () => {
         .request(server)
         .post("/register")
         .send(userDetails)
-       .end((err, res) => {
+        .end((err, res) => {
           if(err){
           return done(err);
-      }
+        }
           res.should.have.status(201);
           done();
         });
@@ -75,7 +75,7 @@ describe("User Registration ", () => {
             if(err){
                 return done(err);
             }
-            res.should.have.status(400);
+            res.should.have.status(201);
             res.body.should.have.property('success').eql(false);
             done();
         });
@@ -152,3 +152,31 @@ it("givenLoginDetails_whenProper_UserLogin_successfully", (done) => {
         });
     });
   });
+
+  describe("forgotPassword", () => {
+    it("givenValidData_whenProper_souldAbleToSendEmailToUserEmail", (done) => {
+      const forgotPasswordDetails = user.user.userForgotPassword;
+      chai.request(server)
+        .post("/forgotPassword")
+        .send(forgotPasswordDetails)
+        .end((error, res) => {
+          if (error) {
+            return done("Invalid details received instead of valid");
+          }
+          res.should.have.status(404);
+          return done();
+        });
+    });
+    it("givenInValidEmail_shouldNotAbleToSendEmailToUserEmail", (done) => {
+      const forgotPasswordDetails = user.user.userForgotPasswordisInvalid;
+      chai.request(server)
+        .post("/forgotpassword")
+        .send(forgotPasswordDetails)
+        .end((error, res) => {
+          if (error) {
+            return done("email-id is empty or unable to fetch details");
+          }
+          return done();
+        });
+    });
+  });  
