9eb24cdeb8dbc1f5d1e7a301d68d2516edcd8c11
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-11-09 13:10:25 +0530

    added: reset password api

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,8 +1,11 @@
 //import controller
 const UserDataController = require("..//controller/user.controller");
-
+const Helper = require("..//utility/user.authenticate");
+const noteController = require("../controller/note.controller");
 module.exports = (app) => {
     app.post('/register', UserDataController.create);
     app.post('/login', UserDataController.login);
-    app.put("/forgotpassword", UserDataController.forgotPassword);
+    app.post("/forgotpassword", UserDataController.forgotPassword);
+    app.post("/resetpassword", Helper.verifyToken, UserDataController.resetPassword);
+    app.post("/createnotes", Helper.verifyToken ,noteController.createNote);
 };
